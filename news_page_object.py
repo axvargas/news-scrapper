e6f88bcedb506571e776fe697f6738bac112b741
Andrés X. Vargas <axvargas@fiec.espol.edu.ec>
2021-07-15 19:19:53 -0500

    Extraction finished

---
+++ news_page_object.py
@@ -0,0 +1,60 @@
+'''
+Created Date: Thursday July 15th 2021 11:14:20 am
+Author: Andrés X. Vargas
+-----
+Last Modified: Thursday July 15th 2021 6:58:48 pm
+Modified By: the developer known as Andrés X. Vargas at <axvargas@fiec.espol.edu.ec>
+-----
+Copyright (c) 2021 MattuApps
+'''
+from common import config
+import requests
+import bs4
+
+
+class NewsPage:
+
+    def __init__(self, news_site_uid, url):
+        self._config = config()['news_sites'][news_site_uid]
+        self._queries = self._config['queries']
+        self._html = None
+
+        self._visit(url)
+
+    def _select(self, query_string):
+        return self._html.select(query_string)
+
+    def _visit(self, url):
+        response = requests.get(url)
+        response.raise_for_status()  # ! in case od error this raises an error
+        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
+
+
+class HomePage(NewsPage):
+
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+
+    @property
+    def article_links(self):
+        link_list = []
+        for link in self._select(self._queries['homepage_article_links']):
+            if link and link.has_attr('href'):
+                link_list.append(link)
+
+        return list(set(link['href'] for link in link_list))
+
+
+class ArticlePage(NewsPage):
+    def __init__(self, news_site_uid, url):
+        super().__init__(news_site_uid, url)
+
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body'])
+        return result[0].text if len(result) else ''
+
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title'])
+        return result[0].text if len(result) else ''
